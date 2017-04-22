98a05e019d1488f485bdda5cf67eb2bf081edf86
level42ca <level42ca@users.noreply.github.comm>
2017-04-22 00:33:26 -0400

    first commit

---
+++ hello.py
@@ -0,0 +1,32 @@
+import master
+
+def hello_world():
+    print("Hello World!")
+
+    print("Nigel is a poopy-but-hole")
+
+    print("Just kidding")
+
+    print("Or am I?")
+
+    print("What?")
+
+    print("Tomato!")
+
+    print("Crazy")
+
+    print("All the honey")
+
+    print("All the D")
+
+    print("This is a really big change")
+
+
+    print("""
+
+    """)
+    selection = input("Type home to return to the main menu: ")
+
+    if selection.lower() == "home":
+        master.master()
+
